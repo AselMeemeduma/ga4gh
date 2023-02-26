a6119968c557801bf48829ebea7e96bc4b781552
Asel Meemeduma <ameemeduma@wiley.com>
2023-02-26 09:30:08 +0530

    Initial commit

---
+++ src/main/java/com/java/org/ga4ghtestcasesforstarterkitdrs/Ga4ghTestCasesForStarterKitDrsApplication.java
@@ -0,0 +1,13 @@
+package com.java.org.ga4ghtestcasesforstarterkitdrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga4ghTestCasesForStarterKitDrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ga4ghTestCasesForStarterKitDrsApplication.class, args);
+	}
+
+}
